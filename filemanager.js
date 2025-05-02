891bee05347615d7998d8df73c46f10cdf025cb4
BrustS <s3rg3yburak@ya.ru>
2025-05-02 20:33:17 +0300

    feat: add ls, cat, rm command

--- filemanager.js
+++ filemanager.js
@@ -2,6 +2,10 @@ import * as readline from 'readline/promises';
 import { showCurrentDirectory, sayHello, sayGoodbye } from "./src/consoleMessages.js";
 import { homedir } from "os";
 import { commandCd } from './src/commands/command_cd.js';
+import { commandLs } from './src/commands/command_ls.js';
+import { commandCat } from './src/commands/command_cat.js';
+import { commandAdd } from './src/commands/command_add.js';
+import { commandRm } from './src/commands/command_rm.js';
 
 const readLine  = readline.createInterface( {
     input: process.stdin,
@@ -18,6 +22,13 @@ readLine.on('line', async (input) => {
         switch (true) {
             case input === 'up': { await commandCd(".."); break; }
             case input.startsWith('cd') : { await commandCd(input.slice(3).trim()); break; }
+            case input === 'ls': { await commandLs(); break; }
+            case input.startsWith('cat') : {await commandCat(input.slice(4).trim()); break; }
+            case input.startsWith('add') : {await commandAdd(input.slice(4).trim()); break; }
+            case input.startsWith('rm') : { await commandRm(input.slice(3).trim()); break; }
+            case input === '.exit' : { readLine.close();
+                process.exit(0);
+                break;}
             default: {
               console.log("Invalid input");
                 break;
